0fdb01d5674be4e4db0460a7af9cc666181f499d
Devanshu <devgoyal952@gmail.com>
2024-08-07 16:20:18 +0530

    imported react-scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../Assets/logo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-  return <nav className="navbar"></nav>;
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="desktopMenu"></div>
+      <Link className="desktopMenuListItem">Home</Link>
+      <Link className="desktopMenuListItem">About</Link>
+      <Link className="desktopMenuListItem">Projects</Link>
+      <Link className="desktopMenuListItem">Contact Me</Link>
+      <button className="desktopMenuBtn">
+        <img src="" alt="" className="desktopMenuImg" /> Contact Me
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
