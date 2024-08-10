4696e85f2694dd96ba0424cdec097c6145d7bb18
Devanshu <devgoyal952@gmail.com>
2024-08-10 18:27:22 +0530

    Added skills section

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "../Skills/Skills.css";
+import gitIcon from "../../Assets/git-new-icon.png";
+import TailwindcssIcon from "../../Assets/tailwindcss-new-icon.png";
+import CssIcon from "../../Assets/css-new-icon.png";
+import HtmlIcon from "../../Assets/html-new-icon.png";
+import JavascriptIcon from "../../Assets/javascript-new-icon.png";
+import ReactIcon from "../../Assets/reactjs-new-icon.png";
+import ReduxIcon from "../../Assets/redux-new-icon.png";
+import GithubIcon from "../../Assets/github-new-icon.png";
+
+const Skills = () => {
+  return (
+    <section id="skills">
+      <span className="skillTitle">What I do</span>
+      <span className="skillDescription">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam iure
+        optio fuga, similique, deserunt culpa veniam consequuntur fugiat quia
+        reiciendis dolore itaque! Quod cum possimus porro consectetur vel totam
+        sunt laboriosam nostrum, odio minima.
+      </span>
+      <div className="skillBars">
+        <div className="skillbar">
+          <div className="html">
+            <img src={HtmlIcon} alt="HTML" className="skillBarImg" /> HTMl
+          </div>
+          <div className="css">
+            <img src={CssIcon} alt="CSS" className="skillBarImg" /> Css
+          </div>
+          <div className="javascript">
+            <img
+              src={JavascriptIcon}
+              alt="Javascript"
+              className="skillBarImg"
+            />
+            Javascript
+          </div>
+          <div className="tailwindcss">
+            <img
+              src={TailwindcssIcon}
+              alt="Tailwindcss"
+              className="skillBarImg"
+            />
+            Tailwindcss
+          </div>
+          <div className="react">
+            <img src={ReactIcon} alt="React" className="skillBarImg" /> React
+          </div>
+          <div className="redux">
+            <img src={ReduxIcon} alt="Redux" className="skillBarImg" /> Redux
+          </div>
+          <div className="git">
+            <img src={gitIcon} alt="Git" className="skillBarImg" /> Git
+          </div>
+          <div className="github">
+            <img src={GithubIcon} alt="Github" className="skillBarImg" /> Github
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
