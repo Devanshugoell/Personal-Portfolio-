f83f27a0ddfcb26793cd16f8faad17a9d80d59a5
Devanshu <devgoyal952@gmail.com>
2024-09-12 17:41:53 +0530

    made website mobile responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,140 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../Assets/logo.png";
 import contactImg from "../../Assets/contact.png";
 import { Link } from "react-scroll";
+import menu from "../../Assets/menu.png";
 
 const Navbar = () => {
+  const [showMenu, setshowMenu] = useState(false);
+
   return (
     <nav className="navbar">
       <img src={logo} alt="logo" className="logo" />
       <div className="desktopMenu">
-        <Link className="desktopMenuListItem">Home</Link>
-        <Link className="desktopMenuListItem">About</Link>
-        <Link className="desktopMenuListItem">Projects</Link>
-        <Link className="desktopMenuListItem">Contact</Link>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Projects
+        </Link>
+        <Link
+          activeClass="active"
+          to="contactPage"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Contact
+        </Link>
       </div>
-      <button className="desktopMenuBtn">
+      <button
+        className="desktopMenuBtn"
+        onClick={() => {
+          document
+            .getElementById("contactPage")
+            .scrollIntoView({ behavior: "smooth" });
+        }}
+      >
         <img src={contactImg} alt="Contact" className="desktopMenuImg" />
         Contact Me
       </button>
+
+      <img
+        src={menu}
+        alt="menu"
+        className="mobMenu"
+        onClick={() => {
+          setshowMenu(!showMenu);
+        }}
+      />
+      <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="listItem"
+          onClick={() => {
+            setshowMenu(false);
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="listItem"
+          onClick={() => {
+            setshowMenu(false);
+          }}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="listItem"
+          onClick={() => {
+            setshowMenu(false);
+          }}
+        >
+          Projects
+        </Link>
+        <Link
+          activeClass="active"
+          to="contactPage"
+          spy={true}
+          smooth={true}
+          // offset={100}
+          duration={500}
+          className="listItem"
+          onClick={() => {
+            setshowMenu(false);
+          }}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
